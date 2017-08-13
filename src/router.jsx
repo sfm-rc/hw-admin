2c21b044a3e7814bb214f7e23d6e795efff15a9a
suyuan <suyuan1573@gmail.com>
2017-08-13 20:59:46 +0800

    build

--- src/router.jsx
+++ src/router.jsx
@@ -3,10 +3,12 @@ import { Router, Route} from 'dva/router';
 import IndexPage from './routes/IndexPage';
 
 // router 路由配置 可使用链接放置参数数据
+
 export default function({ history }) {
     return (
         <Router history={history}>
-            <Route path="/admin-hw" component={IndexPage} />
+             <Route path="/" component={IndexPage} /> 
+            <Route path="/admin-hw/" component={IndexPage} />
             <Route path="/admin-hw/:FeatureId(/:params)" component={IndexPage} />
         </Router>
     );
